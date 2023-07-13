9cf8da7bfc74bdb133f134556d0c91093af20638
EzequielPrincipe <ezeprincipe@live.com>
2023-07-13 00:00:45 -0300

    jsentrega

--- js/script.js
+++ js/script.js
@@ -1,30 +1,41 @@
 function calcularPagos() {
-  let monto = parseInt(document.getElementById("monto").value);
-  let cuotas = parseInt(document.getElementById("cuotas").value);
+  const monto = parseInt(document.getElementById("monto").value);
+  const cuotas = parseInt(document.getElementById("cuotas").value);
 
   if (isNaN(monto) || isNaN(cuotas) || monto <= 0 || cuotas <= 0) {
     console.log("Ingrese un monto y número de cuotas válidos.");
     return;
   }
 
-  let intereses = 0;
+  const intereses = obtenerIntereses(cuotas);
+  const totalConIntereses = calcularTotalConIntereses(monto, intereses);
+  const cuotaMensual = calcularCuotaMensual(totalConIntereses, cuotas);
 
+  mostrarResultados(totalConIntereses, cuotaMensual, cuotas);
+}
+
+function obtenerIntereses(cuotas) {
   switch (cuotas) {
     case 3:
     case 6:
     case 12:
     case 18:
     case 24:
-      intereses = 0.93; // 93% de intereses
-      break;
+      return 0.93; // 59% de intereses
     default:
-      intereses = 0;
-      break;
+      return 0;
   }
+}
 
-  let totalConIntereses = monto * (1 + intereses);
-  let cuotaMensual = totalConIntereses / cuotas;
+function calcularTotalConIntereses(monto, intereses) {
+  return monto * (1 + intereses);
+}
 
+function calcularCuotaMensual(totalConIntereses, cuotas) {
+  return totalConIntereses / cuotas;
+}
+
+function mostrarResultados(totalConIntereses, cuotaMensual, cuotas) {
   console.log(`Monto total a pagar: $${totalConIntereses.toFixed(2)}`);
   console.log(`Cuota mensual: $${cuotaMensual.toFixed(2)}`);
 
